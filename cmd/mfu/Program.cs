03066d6d93965860bb26fe0cdca6c9d3bfb9ee3c
Iskander <iskander.sierra@gmail.com>
2023-09-02 18:45:19 +0200

    chore: Prepare for versioning

--- cmd/mfu/Program.cs
+++ cmd/mfu/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+
+var commands = Environment.GetCommandLineArgs();
+var text = string.Join("; ", commands.Skip(1));
+Console.WriteLine($"Command Line: {text}");
+var assembly = Assembly.GetExecutingAssembly();
+var name = assembly.GetName().Name;
+var info = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+Console.WriteLine($"Program: {name}/{info.InformationalVersion}");
